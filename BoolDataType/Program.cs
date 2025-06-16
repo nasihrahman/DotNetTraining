4cbdd4679bd067358f335fe407baa3a1ab122104
Shalvin P D <shalvin@gmail.com>
2025-06-16 15:59:47 +0530

    Calculator with If else

---
+++ BoolDataType/Program.cs
@@ -0,0 +1,10 @@
+﻿bool isRaining = false;
+
+if (isRaining)
+{
+    Console.WriteLine("Take umbrella");
+}
+else
+{
+    Console.WriteLine("You need not take umbrella");
+}
---
+++ SimpleCalculatorIfElse/Program.cs
@@ -0,0 +1,25 @@
+﻿int i, j, res = 0;
+string oper;
+
+Console.Write("Value 1 : ");
+i  = Int32.Parse(Console.ReadLine());
+
+Console.Write("Value 2 : ");
+j = Int32.Parse(Console.ReadLine());
+
+Console.Write("Enter operator (+, -) : ");
+oper = Console.ReadLine();
+
+if (oper == "+")
+{
+    res = i + j;
+}
+else if (oper == "-")
+{
+    res = i - j;
+}
+else
+{
+    Console.WriteLine("I don't know");
+}
+    Console.WriteLine(res);
