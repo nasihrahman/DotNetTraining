350b0b455b56e7d2d055fa41a051a9a26cbcec4d
Shalvin P D <shalvin@gmail.com>
2025-06-16 14:44:29 +0530

    Int Memory Variable

---
+++ IntMemoryVariable/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int i = 60, j = 20, res;
+res = i + j;
+
+Console.WriteLine(res);
+
+res = i - j;
+Console.WriteLine(res);
+
+res = i * j;
+Console.WriteLine(res);
+
+res = i / j;
+Console.WriteLine(res);
