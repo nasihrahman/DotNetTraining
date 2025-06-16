68dbe80c950fe156da992ed22b53a3f387f1398c
nasihrahman <nasihrahman6@gmail.com>
2025-06-16 15:26:46 +0530

    Add project files.

---
+++ simpleCalc/Program.cs
@@ -0,0 +1,11 @@
+﻿int res, i, j;
+Console.Write("Value 1");
+
+i = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Value 2");
+
+j = Int32.Parse(Console.ReadLine());
+
+res = i + j;
+Console.WriteLine("Result is" +res);
