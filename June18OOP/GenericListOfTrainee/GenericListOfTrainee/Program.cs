c7169466ac1bc3d67c6ccd0a517bbe778ff9e77f
Shalvin P D <shalvin@gmail.com>
2025-06-18 11:31:46 +0530

    Generic List of Class

---
+++ June18OOP/GenericListOfTrainee/GenericListOfTrainee/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericListOfTrainee.Models;
+
+List<Trainee> trainees = new List<Trainee>();
+
+Trainee ashik = new Trainee();
+ashik.Name = "Ashik Aby Joy";
+ashik.Location = "Nettor";
+
+trainees.Add(ashik);
+
+//Object Initialization
+Trainee arjun = new Trainee()
+{
+    Name = "Arjun Akhilash",
+    Location = "Kodunagloor"
+};
+
+trainees.Add(arjun);
+
+foreach (var item in trainees)
+{
+    Console.WriteLine($"{item.Name} \t {item.Location}");
+}
--- June18OOP/TraineeClass/TraineeClass/Program.cs
+++ June18OOP/TraineeClass/TraineeClass/Program.cs
@@ -1,8 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using TraineeClass;
 
-Console.WriteLine("Hello, World!");
-
 Contact neha = new Contact();
 neha.Name = "Neha Prassanan";
 neha.Location = "Cherthala";
