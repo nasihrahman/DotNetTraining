ecd3932b9177456af0b2b24a1f9fd9bc1f0ec89f
Shalvin P D <shalvin@gmail.com>
2025-06-18 10:21:49 +0530

    Contact class with Object Initialization

---
+++ ContactClass/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ContactClass;
+
+Console.WriteLine("Hello, World!");
+
+Contact eldhose = new Contact()
+{
+    Name = "Eldhose",
+    Location = "Kanjiramattom",
+    Points = 70
+};
+Console.WriteLine($"{eldhose.Name} - {eldhose.Location} - {eldhose.Points}");
