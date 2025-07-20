058555b65e500b4a03a11272763d804d5160b509
nasihrahman <nasihrahman6@gmail.com>
2025-07-20 18:47:58 +0530

    Training Files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TocH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ cmdmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cmdmvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
